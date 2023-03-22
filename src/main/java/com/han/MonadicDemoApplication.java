3eb728fae85e31e139d0c513f6e26e6b5b8cfbdf
han <wuhan@leyopharm.com>
2023-03-22 19:06:00 +0800

    初始化

---
+++ src/main/java/com/han/MonadicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.han;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonadicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonadicDemoApplication.class, args);
+    }
+
+}
